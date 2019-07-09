b7201a842b1a324a1ec75b9e63fe0f1697179d44
TCarmine <kappa85t@hotmail.it>
2019-07-09 11:38:02 +0200

    basic setting and gitignore

--- basic_app/index.js
+++ basic_app/index.js
@@ -2,16 +2,12 @@
  * @format
  */
 import React from 'react';
-import ReactNative from 'react-native';
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { Text, AppRegistry } from 'react-native';
 
-const App = () {
-    return (  
-        <Text> as example </Text>
-    );
-}
 
-AppRegistry.registerComponent(appName, () => App);
+const App = () => (  
+        <Text> Some Text example </Text>
+);
+
+AppRegistry.registerComponent('basic_app', () => App);
 
