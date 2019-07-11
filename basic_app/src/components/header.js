03332406d4e876257a7bdc8e5b647bce0266d198
TCarmine <kappa85t@hotmail.it>
2019-07-11 11:05:26 +0200

    npm avvio script and some styling

--- basic_app/src/components/header.js
+++ basic_app/src/components/header.js
@@ -1,22 +1,48 @@
 // import libraries for making component
 import React from 'react';
 import { Text, View } from 'react-native';
+// import {BoxShadow} from 'react-native-shadow';
+
 
 // Make a Component
 const Header = () =>{
-  const { textStyle,viewStyle } = styles;
+  const { textStyle, viewStyle } = styles;
   return(
-    <View style={viewStyle}>
-      <Text style={textStyle}>Altro cambio 3!!!</Text>;
-    </View>
-  )
+
+        <View style={viewStyle} >
+          <Text style={textStyle}>Lingo Match</Text>
+
+        </View>
+  );
 };
 
 const styles ={
-  viewStyle:{
-    backgroundColor:'#F8F8F8';
-  }
+  viewStyle: {
+    // margin: 2,
+    backgroundColor: '#6495ed',/**'#F8F8F8' **/
+    justifyContent:'center',
+    alignItems:'center',
+    height:60,
+    paddingTop: 15,
+    shadowColor:'#F8F8F8',//'#000',
+    shadowOffset:{ width:5, height:20},
+    shadowOpacity:0.2,
+    elevation: 10,
+    position: 'relative'
+  },
+  // androidShadow:{
+  //     width:100,
+  //     height:100,
+  //     color:"#000",
+  //     border:2,
+  //     radius:3,
+  //     opacity:0.2,
+  //     x:0,
+  //     y:3,
+  //     style:{marginVertical:5}
+  // },
   textStyle: {
+
     fontSize:20
   }
 };
