d0955ecd39bfe1bda532c8798c5e6d2357b7796d
TCarmine <kappa85t@hotmail.it>
2019-07-12 15:39:40 +0200

    refactor from functional component to class component and some debuging

--- basic_app/src/components/AlbumList.js
+++ basic_app/src/components/AlbumList.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
+
 class AlbumList extends Component  {
+    componentWillMount(){
+    console.log('componentWillMount in AlbumList');
+    debugger;
+    } 
     render() {
         return (
-        <View>
-            <Text> These are amazing albums: </Text>
-        </View>
+            <View>
+                <Text> These are amazing albums: </Text>
+            </View>
          );
     }
 }
