d61a5d518d5fa83d7986046d42450fe3dd6284e0
TCarmine <kappa85t@hotmail.it>
2019-07-15 10:10:27 +0200

    added axios and displayed list album

--- basic_app/src/components/AlbumList.js
+++ basic_app/src/components/AlbumList.js
@@ -3,19 +3,30 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 
-
 class AlbumList extends Component  {
+    // Component State
+    // class level proprety
+    state = { albums: [] }  //1. default this.hasstate()
+    
     componentDidMount(){
        // return a promise as exios is async
        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-         .then(response=>console.log(response));      
-    } 
+         .then(response => this.setState({ albums: response.data }));  
+        
+    }
+
+    renderAlbums() {
+       return this.state.albums.map(album => <Text>{ album.title }</Text>);
+    }
+    
     render() {
+        
         return (
             <View>
-                <Text> These are amazing albums: </Text>
+                {this.renderAlbums()}
+                
             </View>
-         );
+        );
     }
 }
 
