7ca5805e13ec99ed932458c72972f5edbc7b191a
TCarmine <kappa85t@hotmail.it>
2019-07-30 17:07:22 +0200

    added a Card component with some style

--- basic_app/src/components/AlbumList.js
+++ basic_app/src/components/AlbumList.js
@@ -3,24 +3,30 @@ import { View } from 'react-native';
 import axios from 'axios';
 import AlbumDetail from './AlbumDetail';
 
+
 class AlbumList extends Component  {
-    // Component State
+   
     // class level proprety
-    state = { albums: [] }  //1. default this.hasstate()
-    
+     // Component State, make sure that everytime data change the component will remount, this is how
+     // React behave once the state change
+    state = { albums: [] }  //1. default, when a component is created has a state this.state()
+    // ComponentWillMpunt is been deprecated
     componentDidMount(){
        // return a promise as exios is async
        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
          .then(response => this.setState({ albums: response.data }));  
         
     }
-
+ 
     renderAlbums() {
-       return this.state.albums.map(
-           album => <AlbumDetail key={album.title} albumrecord = {album} />
+        return this.state.albums.map(album => 
+            //albumrecord--> passing props to the child
+          <AlbumDetail key={album.title} albumrecord = {album}/ >
+
         );
+        
     }
-    
+   
     render() {
         
         return (
