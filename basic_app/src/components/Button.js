0bf604263753bb0e4a385ce0c8e1c127fa8d88c5
TCarmine <kappa85t@hotmail.it>
2019-07-31 22:52:28 +0200

    added button componend and styled it

--- basic_app/src/components/Button.js
+++ basic_app/src/components/Button.js
@@ -4,9 +4,10 @@ import { Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
     
+    const { buttonStyle,textStyle } = styles;
     return (
-        <TouchableOpacity>
-            <Text>
+        <TouchableOpacity style={buttonStyle}>
+            <Text style = { textStyle }>
                 Buy this Album
             </Text>
         </TouchableOpacity>    
@@ -14,4 +15,25 @@ const Button = () => {
 
 };
 
+const styles = {
+
+    textStyle:{
+        alignSelf:'center',
+        color:'#007aff',
+        fontSize:16,
+        fontWeight:'600',
+        paddingTop:10,
+        paddingBottom:10
+    },
+    buttonStyle:{
+        flex:1,
+        alignSelf:'stretch',
+        backgroundColor:'#fff',
+        borderRadius:5,
+        borderWidth:1,
+        borderColor:'#007aff',
+        marginLeft:5,
+        marginRight:5
+   }
+}
 export default Button;
