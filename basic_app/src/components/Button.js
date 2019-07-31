36095f0694e6f4ccbf9cf6f4a4cbbb2ee9b538d7
TCarmine <kappa85t@hotmail.it>
2019-07-31 16:59:35 +0200

    same as previous

---
+++ basic_app/src/components/Button.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+
+const Button = () => {
+    
+    return (
+        <TouchableOpacity>
+            <Text>
+                Buy this Album
+            </Text>
+        </TouchableOpacity>    
+    );
+
+};
+
+export default Button;
