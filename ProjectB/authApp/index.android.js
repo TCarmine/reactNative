db874e9c41ed52eb8ef0e043958a4fa62e96eb89
TCarmine <kappa85t@hotmail.it>
2019-08-03 10:02:04 +0200

    useful comments for configurations purpose

--- ProjectB/authApp/index.android.js
+++ ProjectB/authApp/index.android.js
@@ -1,5 +1,10 @@
 //This should enable to debug CSS from Chrome react dev tool 
-require('react-devtools-core').connectToDevTools({host: '192.168.43.151'})
+// It happens that app.json created by default react-native is not been 
+// read by Adroid studio, so it never recognize the mapping of the app name,
+// so better use app's string name:'authApp'.
+// on top of that this file is not read once the build is done, so metro does not
+// read and update version, so better check manually into Android Studio
+require('react-devtools-core').connectToDevTo ols({host: '192.168.43.151'})
 
 /**
  * @format
