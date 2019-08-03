490c5758410158971391c3f4b6fc8ab5495576d1
TCarmine <kappa85t@hotmail.it>
2019-08-03 22:43:18 +0200

    setState for TextInput

--- ProjectB/authApp/src/components/LoginForm.js
+++ ProjectB/authApp/src/components/LoginForm.js
@@ -1,11 +1,25 @@
 import React, {Component} from 'react';
+import { TextInput } from 'react-native';
 import { Button, Card, CardSection} from './common';
 
 class LoginForm extends Component{
+
+    state = { text: ''};
+
+
     render(){
         return(
             <Card>
-                <CardSection />
+                <CardSection>
+                 {/* 
+                 By default TextInput does not have width and heigth(0), like image
+                */}
+                <TextInput 
+                    value={this.state.text}
+                    onChangeText = { text => this.setState({ text })}
+                    style={{ height:50, width:150}}/>   
+                </CardSection>
+
                 <CardSection />
                 
                 <CardSection>
